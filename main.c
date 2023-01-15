50bdb9f8d03d85d70e7d5bb22b38df4ce93518d0
maykonjuso <maykola1331@gmail.com>
2023-01-15 20:26:06 -0300

    final.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Substitui quebra de linha pelo caractere nulo
 void remover(char *str)
 {
     int comprimento = strlen(str);
@@ -12,11 +13,13 @@ void remover(char *str)
     }
 }
 
+// Função para contar quantidade de linhas do arquivo csv
 int contarLinhas(char *nome)
 {
-
+    // Criando ponteiro para ler o arquivo
     FILE *fp = fopen(nome, "r");
 
+    // Avisar caso ocorra algum erro na abertura do arquivo
     if (fp == NULL)
     {
         printf("Não foi possível abrir o arquivo\n");
@@ -26,48 +29,59 @@ int contarLinhas(char *nome)
     char c;
     int count = 0;
 
+    // Percorre o arquivo até encontrar o caractere EOF
     for (c = getc(fp); c != EOF; c = getc(fp))
     {
-        if (c == '\n') // Increment count if this character is newline
+        // Incrementa o contador caso o seja uma quebra de linha
+        if (c == '\n')
             count = count + 1;
     }
 
+    // Fecha o ponteiro
     fclose(fp);
+
+    // Retorna a quantidade de linhas
     return count;
 }
 
-int main(int argc, char **argv)
+int main()
 {
+    // Declara uma struct com duas linhas (valor e categoria)
     typedef struct
     {
-        char categoria[50];
         double valor;
-    } venda;
+        char categoria[100];
+    } VENDA;
 
-    int tamLinhas = contarLinhas("dadosAmostra.csv");
+    // Chama a função contarLinhas para salvar a quantidade de linhas do arquivo na variável tamLinhas
+    int tamLinhas = contarLinhas("dados.csv");
 
-    // Abrir o arquivo .csv
-
+    // Abertura dos arquivos
     FILE *fp, *fsaida;
-    fp = fopen("dadosAmostra.csv", "r");
-    fsaida = fopen("dadosSaidaAmostra.csv", "w");
+    fp = fopen("dados.csv", "r");
+    fsaida = fopen("dadosSaida.csv", "w");
 
     if (fp == NULL)
     {
+        // Condição caso não seja possível abrir o arquivo
         printf("Não foi possível abrir o arquivo\n");
         return 1;
     }
 
+    // Alocando memória dinamicamente para o vetor de vendas
+    VENDA *vendas = (VENDA *)malloc(tamLinhas * sizeof(VENDA));
+
     // Lê o arquivo linha por linha
-    venda vendas[tamLinhas];
     char linha[100];
     int tam = 0;
 
+    // Preencher struct vendas com os dados do csv
     while (fgets(linha, sizeof(linha), fp))
     {
+        // Divide a linha em tokens usando ";" como delimitador
         char *valor_str = strtok(linha, ";");
         char *categoria = strtok(NULL, ";");
-        // Divide a linha em tokens usando ";" como delimitador
+
         remover(categoria);
 
         strcpy(vendas[tam].categoria, categoria);
@@ -77,41 +91,55 @@ int main(int argc, char **argv)
         tam++;
     }
 
-    // SelectionSort
+    // Usando selectionSort para ordenar arquivos
+
+    // Percorre toda extensão do vetor
     for (int i = 0; i < tamLinhas - 1; i++)
     {
+        // Armazena uma posição i do vetor em min
         int min = i;
+
+        // O laço será percorrido a partir do proximo valor de i
         for (int j = i + 1; j < tamLinhas; j++)
         {
+            // Essa linha compara a posição J com min, e atualiza o valor
             if (vendas[j].valor < vendas[min].valor)
             {
                 min = j;
             }
         }
-        venda tmp = vendas[i];
+        // armazena a posição atual temporariamente
+        VENDA tmp = vendas[i];
+        // Adiciona a posição menor na posição do vetor
         vendas[i] = vendas[min];
+        // Adiciona a variavel temporaria que havia o valor menor
         vendas[min] = tmp;
     }
 
-    // inicio da contagem
+    // inicia uma contagem
     clock_t start_time = clock();
 
     for (int i = tamLinhas - 1; i >= 0; i--)
     {
-        // gera e preenche o arquivo de saída
+        // Preenche o arquivo de saída com o vetor ordenado
         fprintf(fsaida, "%.2lf %s\n", vendas[i].valor, vendas[i].categoria);
 
-        // printa no console - temporário
-        printf("%.2lf %s\n", vendas[i].valor, vendas[i].categoria);
+        // Printa no console - temporário (comentado para não alterar o tempo de processamento)
+        // printf("%.2lf %s\n", vendas[i].valor, vendas[i].categoria);
     }
 
+    // Encerra arquivos e desaloca a memória alocada em malloc
     fclose(fp);
     fclose(fsaida);
+    free(vendas);
 
-    // final da contagem
+    // Final da contagem
     clock_t end_time = clock();
-    // calculo do tempo
+
+    // Calculo do tempo
     double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    // Exibe o tempo de processamento
     printf("Tempo de processamento: %.2f segundos", elapsed_time);
 
     return 0;
